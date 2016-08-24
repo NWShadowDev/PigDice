1aefd005e92c18365195a0273abeaa5e058c49cd
Amber Farrington and Addison Nishijima <addison@>
2016-08-24 16:40:38 -0700

    add potential functions and methods that need work

--- js/scripts.js
+++ js/scripts.js
@@ -37,20 +37,14 @@ $(document).ready(function () {
     event.preventDefault();
     var player1Name = $("#firstname").val();
     var player2Name = $("#secondname").val();
-    if (newPlayer1 == null) {
-    alert("please enter a name");
-    return false;
-  }
-    else {
-      return true;
-    }
-    if (newPlayer2 == null) {
-    alert("please enter a name");
-    return false;
-  }
-    else {
-      return true;
-    }
+    // if (newPlayer1 == null) {
+    // alert("please enter a name");
+    // return false;
+  // }
+  //   if (newPlayer2 == null) {
+  //   alert("please enter a name");
+  //   return false;
+  // }
     $("#balance").text(currentBalance1);
     $("#balance").text(currentBalance2);
     newPlayer1 = new Player1(currentBalance1, player1Name);
@@ -96,7 +90,7 @@ $(document).ready(function () {
      document.getElementById("roll1").disabled = false;
      alert("Sorry, you lost your turn! Player 1, your turn!");
    }else {
-     newPlayer2.hold(currentBalance2 += inputtedRoll2);//problem
+     newPlayer2.hold(currentBalance2 += inputtedRoll2);
      alert("Player2 rolled: " + inputtedRoll2);
      $("#secondbalance").text(currentBalance2);
    }
