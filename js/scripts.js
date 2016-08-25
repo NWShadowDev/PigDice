a3b5594d5c3814841881a920b2982af4dac2e511
Amber Farrington and Addison Nishijima <amber@>
2016-08-25 14:46:13 -0700

    fix buttons for winning and losing turns

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,9 @@ function Player2 (initial, name) {
   this.balance = initial;
   this.name = name;
 }
+// function isWinner(balance){
+//   this.balance = 20;
+// }
 
 function isEmpty(value){
     return (value === undefined || value == null || value.length <= 0) ? true : false;
@@ -21,6 +24,10 @@ Player1.prototype.loseturn = function(amount) {
   this.balance -= amount;
 }
 
+Player1.prototype.winner = function(amount) {
+  this.balance = 10;
+}
+
 Player2.prototype.hold = function(amount) {
   this.balance += amount;
 }
@@ -28,6 +35,11 @@ Player2.prototype.hold = function(amount) {
 Player2.prototype.loseturn = function(amount) {
   this.balance -= amount;
 }
+
+Player2.prototype.winner = function(amount) {
+  this.balance = 10;
+}
+
 
 //user interface logic
 var newPlayer1;
@@ -44,7 +56,6 @@ $(document).ready(function () {
     if (player1Name == "") {
     alert("Please enter a name or be fed to the pigs!");
   }else {
-
   }
     if (player2Name == "") {
     alert("Please enter a name or be fed to the pigs!");
@@ -58,59 +69,81 @@ $(document).ready(function () {
     $("#player1Name").text(player1Name);
     $("#player2Name").text(player2Name);
   });
-
+//Player 1 Roll//
   $("#roll1").click(function(event){
+    debugger;
   event.preventDefault();
   var inputtedRoll1 = Math.floor(Math.random() * 6);
-   if (inputtedRoll1 === 1) {
+   if (inputtedRoll1 <= 1) {
      newPlayer1.loseturn();
      currentBalance1 = 0;
      $("#firstbalance").text(currentBalance1);
      alert("Sorry, you lost your turn! Player 2, your turn!");
      document.getElementById("roll1").disabled = true;
      document.getElementById("roll2").disabled = false;
-   }else if (inputtedRoll1 === 0) {
-      newPlayer1.loseturn();
-        currentBalance1 = 0;
-        $("#firstbalance").text(currentBalance1);
-        alert("Sorry, you lost your turn! Player 2, your turn!");
-        document.getElementById("roll1").disabled = true;
-        document.getElementById("roll2").disabled = false;
-  } else {
-     newPlayer1.hold(currentBalance1 +=inputtedRoll1);
-     alert("Player1 rolled: " + inputtedRoll1);
-     $("#firstbalance").text(currentBalance1);
+   }else{
+     newPlayer1.hold(currentBalance1 += inputtedRoll1);
+     alert("Player1 rolled:" + inputtedRoll1);
+      $("#firstbalance").text(currentBalance1);
+      document.getElementById("roll2").disabled = true;
+      document.getElementById("roll1").disabled = false;
+   }
+   if(currentBalance1 >= 10) {
+     newPlayer1.winner();
+     alert("Player 1, you win! Player 2, the pigs will now eat you! >:D");
    }
   $("#p1diceroll").text(newPlayer1.balance);
   $("#firstname").val("");
   $("#secondname").val("");
   });
 
+  $("#skip1").click(function(event) {
+    newPlayer1.hold(currentBalance1 += inputtedRoll1);
+    if(inputtedRoll1 >= 2){
+    alert("Player1 rolled: " + inputtedRoll1);
+    $("#firstbalance").text(currentBalance1);
+  }
+    if(currentBalance1 >= 10) {
+      newPlayer1.winner();
+      alert("Player 1, you win! Player 2, the pigs will now eat you! >:D");
+    }
+  });
+
   //Player2//
   $("#roll2").click(function(event){
+    debugger;
   event.preventDefault();
   var inputtedRoll2 = Math.floor(Math.random() * 6);
-   if (inputtedRoll2 ===1) {
+   if (inputtedRoll2 <=1) {
      newPlayer2.loseturn();
      currentBalance2 = 0;
-     document.getElementById("roll2").disabled = true;
-     document.getElementById("roll1").disabled = false;
      $("#secondbalance").text(currentBalance2);
      alert("Sorry, you lost your turn! Player 1, your turn!");
-   }else if (inputtedRoll2 === 0) {
-     newPlayer2.loseturn();
-     currentBalance2 = 0;
      document.getElementById("roll2").disabled = true;
      document.getElementById("roll1").disabled = false;
-     $("#secondbalance").text(currentBalance2);
-     alert("Sorry, you lost your turn! Player 1, your turn!");
-   }else {
+   }else{
      newPlayer2.hold(currentBalance2 += inputtedRoll2);
-     alert("Player2 rolled: " + inputtedRoll2);
-     $("#secondbalance").text(currentBalance2);
+     alert("Player2 rolled:" + inputtedRoll2);
+      $("#secondbalance").text(currentBalance2);
+      document.getElementById("roll1").disabled = true;
+      document.getElementById("roll2").disabled = false;
+   }
+   if(currentBalance2 >= 10) {
+     newPlayer2.winner();
+     alert("Player 2, you win! Player 1, the pigs will now eat you! >:D");
    }
   $("#p2diceroll").text(newPlayer2.balance);
   $("#firstname").val("");
   $("#secondname").val("");
+  });
+
+  $("#skip2").click(function(event){
+    newPlayer2.hold(currentBalance2 += inputtedRoll2);
+    alert("Player2 rolled: " + inputtedRoll2);
+    $("#secondbalance").text(currentBalance2);
+    if(currentBalance2 >= 10) {
+      newPlayer2.winner();
+      alert("Player 2, you win! Player 1, the pigs will now eat you! >:D")
+    }
   });
 });
